a400126e950e56492540e1e4de93c5b5599af418
Alex Zavatone <zav@mac.com>
2020-04-05 08:02:27 -0500

    Initial Commit

---
+++ Nam COVID-19 Self Reporting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Nam COVID-19 Self Reporting
+//
+//  Created by Alex Zavatone on 4/5/20.
+//  Copyright © 2020 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Nam COVID-19 Self Reporting/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Nam COVID-19 Self Reporting
+//
+//  Created by Alex Zavatone on 4/5/20.
+//  Copyright © 2020 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
